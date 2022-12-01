13293852c62a1d744121deb16fbd9dab004ca2d3
Austin Walterscheid <aus.10walterscheid@gmail.com>
2022-11-30 17:02:12 -0700

    Final upload

--- src/App.js
+++ src/App.js
@@ -3,18 +3,60 @@ import Square from './components/Square';
 import './App.css';
 
 function App() {
+
   const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
+  function calculateWinner(arr) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            arr[a] &&
+            arr[a] === arr[b] &&
+            arr[a] === arr[c]
+        ) {
+            return `${arr[a]} won!`;
+        }
+    }
+    return "Who will win?";
+}
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    setPlayer(true)
+
+}
   return (
     <div className="App">
-      <Square squares={squares}
-        setSquares={setSquares}
-        player={player}
-        setPlayer={setPlayer}></Square>
+      <button onClick={handleClick}>Reset</button>
+        <div className="container">
+            {squares.map((val, index) => {
+                return (
+                    <Square
+                        setSquares={setSquares}
+                        index={index}
+                        squareValue={val}
+                        squares={squares}
+                        player={player}
+                        setPlayer={setPlayer}
+                    />
+                );
+            })}
+            
+        </div>
+        <span>{calculateWinner(squares)}</span>
     </div>
-  );
+);
 }
-console.log(props.squares, props.player)
+
 
 
 export default App;
